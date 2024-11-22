60a6cb1a64b6ee96393af2957486924e85b0db09
Roopasaontharya <143249730+Roopasaontharya@users.noreply.github.com>
2024-11-22 11:50:42 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,45 @@
+// public class hello {
+//     public static void main(String[] args) {
+//         // Print "Hello, World!" to the console
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+import java.util.Scanner;
+
 public class hello {
     public static void main(String[] args) {
-        // Print "Hello, World!" to the console
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        // Asking for the number of rows for the upper half of the diamond
+        System.out.print("Enter the number of rows for the diamond pattern: ");
+        int n = scanner.nextInt();
+        
+        // Print the upper half of the diamond
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        // Print the lower half of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // Print leading spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
+
